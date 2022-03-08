dcec6a87c477eb24f34391bb699f89b49d9bb9dc
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-08 14:00:39 +0600

    use local storage

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
+const getCurrnetStatus = () => {
+    const status = localStorage.getItem('active-status');
+    if (status == 'Play') {
+        document.getElementById('toggle-button').innerText = "Play";
+        console.log('Play');
+
+    } else {
+        document.getElementById('toggle-button').innerText = "Pause";
+        console.log('Pause');
+
+    }
+
+}
+getCurrnetStatus();
+
 document.getElementById('toggle-button').addEventListener('click', () => {
     let currentText = document.getElementById('toggle-button').innerText;
     if (currentText == "Play") {
-        // localStorage.setItem('active-status', 'Pause');
-        sessionStorage.setItem('active-status', 'Pause');
-        currentText = sessionStorage.getItem('active-status');
+        localStorage.setItem('active-status', 'Pause');
+        // // sessionStorage.setItem('active-status', 'Pause');
+        currentText = localStorage.getItem('active-status');
     } else {
-        // localStorage.setItem('active-status', 'Play');
-        sessionStorage.setItem('active-status', 'Play');
+        localStorage.setItem('active-status', 'Play');
+        // // sessionStorage.setItem('active-status', 'Play');
 
-        currentText = sessionStorage.getItem('active-status');
+        currentText = localStorage.getItem('active-status');
 
     }
     document.getElementById('toggle-button').innerText = currentText;
